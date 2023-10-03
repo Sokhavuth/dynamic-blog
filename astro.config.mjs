6afc6cf09b715f797b0993c98491cf4c20ea3df7
Sokhavuth <vuthdevelop@gmail.com>
2023-10-03 10:13:38 +0700

    Make it better

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [tailwind()]
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  })]
 });
